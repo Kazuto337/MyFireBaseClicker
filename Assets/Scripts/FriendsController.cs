2981bb35d1ebc60e072c1e83f761cef6177adac1
Andresjales <andresjales@gmail.com>
2022-10-27 02:16:33 -0500

    FriendsController

---
+++ Assets/Scripts/FriendsController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using Firebase.Database;
+using UnityEngine.UI;
+
+public class FriendsController : MonoBehaviour
+{
+
+}
